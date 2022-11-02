16d9b65c0376666e4b4e92999b36d0b09a0c21ae
idvrifat <idvrifat@gmail.com>
2022-11-02 07:32:07 +0600

    basic node mongo setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middle wares
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vgiybbh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+app.get('/', (req, res) =>{
+    console.log("Genius Car Server Running")
+})
+
+app.listen(port, () => {
+    console.log(`Genius Card Server Running At Port ${port}`)
+})
