37dbcb8bb17486a4fc265c6ed4db15ba66e0afe8
idvrifat <idvrifat@gmail.com>
2022-11-05 11:55:05 +0600

    First Commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require ('express')
+const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 
 // middle wares
@@ -13,21 +13,115 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vgiybbh.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message : 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(401).send({message : 'Unauthorized Access'})  
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+async function run() {
+
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        })
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // orders api
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('inside orders api', decoded);
+            if(decoded.email !== req.query.email){
+                
+            }
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log(order)
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query);
+            res.send(result);
+        });
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     console.log("Genius Car Server Running")
 })
 
 app.listen(port, () => {
-    console.log(`Genius Card Server Running At Port ${port}`)
+    console.log(`Genius Car Server Running At Port ${port}`)
 })
